9adfff140cd8a34f48bc3f4ff8a233628237948c
Yurii Oksamytnyi <141469505+YuriiOks@users.noreply.github.com>
2024-03-15 10:44:33 +0000

    Create example_1.py

---
+++ examples/example_1.py
@@ -0,0 +1,7 @@
+# Prompt the user to enter the number of times to print the message
+n = int(input("Enter the number of times to print the message: "))
+
+# Use a for loop to repeat the following block of code 'n' times
+for _ in range(n):
+    # Print the message "Hello, Python!"
+    print("Hello, Python!")
