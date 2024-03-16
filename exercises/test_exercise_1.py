7b7538b7379934ac3b93b71cf681a871e0977d44
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-16 23:23:38 +0000

    Update test_exercise_1.py

--- exercises/test_exercise_1.py
+++ exercises/test_exercise_1.py
@@ -9,19 +9,19 @@ class TestExercise1(unittest.TestCase):
         return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
 
     def test_countdown_5(self):
-        output = self.run_exercise("5\\n")
-        expected_output = "5\\n4\\n3\\n2\\n1\\nStart!\\n"
-        self.assertEqual(output, expected_output)
+        output = self.run_exercise("5\n")
+        expected_output = "5\n4\n3\n2\n1\nStart!\n"
+        self.assertIn(output, expected_output)
 
     def test_countdown_3(self):
-        output = self.run_exercise("3\\n")
-        expected_output = "3\\n2\\n1\\nStart!\\n"
-        self.assertEqual(output, expected_output)
+        output = self.run_exercise("3\n")
+        expected_output = "3\n2\n1\nStart!\n"
+        self.assertIn(output, expected_output)
 
     def test_countdown_1(self):
-        output = self.run_exercise("1\\n")
-        expected_output = "1\\nStart!\\n"
-        self.assertEqual(output, expected_output)
+        output = self.run_exercise("1\n")
+        expected_output = "1\nStart!\n"
+        self.assertIn(output, expected_output)
 
 
 if __name__ == '__main__':
