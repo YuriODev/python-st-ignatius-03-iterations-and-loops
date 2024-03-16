e98860aac5afb80a76c8541dec189fce208a8f5e
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-16 02:04:48 +0000

    Update test_exercise_20.py

--- exercises/test_exercise_20.py
+++ exercises/test_exercise_20.py
@@ -2,6 +2,7 @@
 import unittest
 import subprocess
 import os
+import ast
 
 class TestExercise20(unittest.TestCase):
     def run_exercise(self, input_value):
@@ -22,6 +23,24 @@ class TestExercise20(unittest.TestCase):
         output = self.run_exercise("5\n")
         expected_output = "1 3 5\n"
         self.assertEqual(output, expected_output)
+
+
+    def chek_list_usage(self):
+        with open("exercise_20.py", "r") as source_code:
+            tree = ast.parse(source_code.read())
+            for node in ast.walk(tree):
+                if isinstance(node, (ast.List, ast.ListComp)):
+                    self.fail("List usage found in the code.")
+                elif isinstance(node, ast.Call) and isinstance(node.func, ast.Name) and node.func.id == 'list':
+                    self.fail("List usage found in the code.")
+
+    def test_no_list_usage(self):
+        with open("exercise_20.py", "r") as source_code:
+            code = source_code.read()
+            self.assertNotIn("list", code)
+            self.assertNotIn("[", code)
+            self.assertNotIn("]", code)
+
 
 if __name__ == '__main__':
     unittest.main()
