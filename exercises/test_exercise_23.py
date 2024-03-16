ec7e5cca280844d38b38a7816369c9bf97a2383b
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-16 02:09:28 +0000

    Update test_exercise_23.py

--- exercises/test_exercise_23.py
+++ exercises/test_exercise_23.py
@@ -2,6 +2,7 @@
 import unittest
 import subprocess
 import os
+import ast
 
 class TestExercise23(unittest.TestCase):
     def run_exercise(self, input_values):
@@ -19,6 +20,24 @@ class TestExercise23(unittest.TestCase):
     def test_average_sequence_10_to_50_by_10_ending_with_0(self):
         output = self.run_exercise("10\n20\n30\n40\n50\n0\n")
         self.assertAlmostEqual(float(output.strip()), 30.0, places=1)
+
+    def chek_list_usage(self):
+        with open("exercise_23.py", "r") as source_code:
+            tree = ast.parse(source_code.read())
+            for node in ast.walk(tree):
+                if isinstance(node, (ast.List, ast.ListComp)):
+                    self.fail("List usage found in the code.")
+                elif isinstance(node, ast.Call) and isinstance(node.func, ast.Name) and node.func.id == 'list':
+                    self.fail("List usage found in the code.")
+
+    def test_no_list_usage(self):
+        with open("exercise_23.py", "r") as source_code:
+            code = source_code.read()
+            self.assertNotIn("list", code)
+            self.assertNotIn("[", code)
+            self.assertNotIn("]", code)
+
+    
 
 if __name__ == '__main__':
     unittest.main()
