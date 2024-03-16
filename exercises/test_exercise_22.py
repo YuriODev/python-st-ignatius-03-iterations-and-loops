e3c6472773bbe431756a906203953877543d41c6
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-16 02:07:35 +0000

    Update test_exercise_22.py

--- exercises/test_exercise_22.py
+++ exercises/test_exercise_22.py
@@ -2,6 +2,7 @@
 import unittest
 import subprocess
 import os
+import ast
 
 class TestExercise22(unittest.TestCase):
     def run_exercise(self, input_value):
@@ -22,6 +23,30 @@ class TestExercise22(unittest.TestCase):
         output = self.run_exercise("987654\n")
         expected_output = "98765\n9876\n987\n98\n9\n"
         self.assertEqual(output, expected_output)
+
+    def check_string_index_usage(self):
+        with open("exercise_22.py", "r") as source_code:
+            tree = ast.parse(source_code.read())
+            for node in ast.walk(tree):
+                if isinstance(node, ast.Subscript) and isinstance(node.value, ast.Name) and node.value.id == 'number':
+                    self.fail("String index usage found in the code.")
+
+    def chek_list_usage(self):
+        with open("exercise_22.py", "r") as source_code:
+            tree = ast.parse(source_code.read())
+            for node in ast.walk(tree):
+                if isinstance(node, (ast.List, ast.ListComp)):
+                    self.fail("List usage found in the code.")
+                elif isinstance(node, ast.Call) and isinstance(node.func, ast.Name) and node.func.id == 'list':
+                    self.fail("List usage found in the code.")
+
+    def test_no_list_usage(self):
+        with open("exercise_22.py", "r") as source_code:
+            code = source_code.read()
+            self.assertNotIn("list", code)
+            self.assertNotIn("[", code)
+            self.assertNotIn("]", code)
+
 
 if __name__ == '__main__':
     unittest.main()
