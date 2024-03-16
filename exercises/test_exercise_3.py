19f4edf940ff620fd83a88ca7f8b2855a4dde3a3
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-16 23:37:50 +0000

    Update test_exercise_3.py

--- exercises/test_exercise_3.py
+++ exercises/test_exercise_3.py
@@ -9,18 +9,18 @@ class TestExercise3(unittest.TestCase):
         return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
 
     def test_sequence_to_25(self):
-        output = self.run_exercise("25\\n")
-        expected_output = "20 21 22 23 24 25\\n"
+        output = self.run_exercise("25\n")
+        expected_output = "20 21 22 23 24 25\n"
         self.assertEqual(output, expected_output)
 
     def test_sequence_to_30(self):
-        output = self.run_exercise("30\\n")
-        expected_output = "20 21 22 23 24 25 26 27 28 29 30\\n"
+        output = self.run_exercise("30\n")
+        expected_output = "20 21 22 23 24 25 26 27 28 29 30\n"
         self.assertEqual(output, expected_output)
 
     def test_sequence_to_20(self):
-        output = self.run_exercise("20\\n")
-        expected_output = "20\\n"
+        output = self.run_exercise("20\n")
+        expected_output = "20\n"
         self.assertEqual(output, expected_output)
 
 
