ac3d34fa951c2661ddb0e764d524bb76fd5743be
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-16 02:43:41 +0000

    Update test_exercise_47.py

--- exercises/test_exercise_47.py
+++ exercises/test_exercise_47.py
@@ -25,7 +25,7 @@ import os
 import ast
 
 
-class TestExercise37(unittest.TestCase):
+class TestExercise47(unittest.TestCase):
     def run_exercise(self, input_values):
         exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_47.py")
         return subprocess.check_output(['python3', exercise_file_path], input=input_values, text=True, universal_newlines=True)
