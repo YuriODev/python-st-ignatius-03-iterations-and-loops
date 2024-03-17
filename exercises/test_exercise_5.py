c417bd0752dc8349a21030382a93a77a5f2386b2
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-17 00:02:19 +0000

    Update test_exercise_5.py

--- exercises/test_exercise_5.py
+++ exercises/test_exercise_5.py
@@ -2,6 +2,7 @@
 import unittest
 import subprocess
 import os
+import ast
 
 class TestExercise5(unittest.TestCase):
     def run_exercise(self, input_value):
@@ -22,6 +23,13 @@ class TestExercise5(unittest.TestCase):
         output = self.run_exercise("1\n")
         expected_output = "1\n"
         self.assertEqual(output, expected_output)
+
+    def test_no_loop_usage(self):
+        with open(os.path.join(os.path.dirname(__file__), "exercise_5.py"), "r") as file:
+            content = file.read()
+            print(content)
+            self.assertTrue(("for" in content) or ("while" in content), "You should use a loop for this exercise")
+
 
 if __name__ == '__main__':
     unittest.main()
