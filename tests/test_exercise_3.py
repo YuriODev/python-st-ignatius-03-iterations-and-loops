c3263739698599c7c76dbb4c17f7d8853fa369bf
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-23 23:01:11 +0000

    Update test_exercise_3.py

--- tests/test_exercise_3.py
+++ tests/test_exercise_3.py
@@ -5,23 +5,32 @@ from .test_utils import CustomTestCase, CustomTestRunner
 class TestExercise3(CustomTestCase):
 
     def test_sequence_to_25(self):
+        """
+        The program should print the numbers from 20 to 25.
+        """
         inputs = ["25"]
         output = self.run_exercise(inputs)
-        expected_output = "20 21 22 23 24 25"
+        expected_output = "20 21 22 23 24 25 \n"
         self.assertInCustom(expected=expected_output, actual=output,
                             input_value=inputs)
 
     def test_sequence_to_30(self):
+        """
+        The program should print the numbers from 20 to 30.
+        """
         inputs = ["30"]
         output = self.run_exercise(inputs)
-        expected_output = "20 21 22 23 24 25 26 27 28 29 30"
+        expected_output = "20 21 22 23 24 25 26 27 28 29 30 \n"
         self.assertInCustom(expected=expected_output, actual=output,
                             input_value=inputs)
 
     def test_sequence_to_20(self):
+        """
+        The program should print the number 20.
+        """
         inputs = ["20"]
         output = self.run_exercise(inputs)
-        expected_output = "20"
+        expected_output = "20 \n"
         self.assertInCustom(expected=expected_output, actual=output,
                             input_value=inputs)
 
