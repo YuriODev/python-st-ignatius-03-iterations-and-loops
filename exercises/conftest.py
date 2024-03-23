af3d2a6a7fa2923b7de89cfd6445bc5c119c4389
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-23 20:53:36 +0000

    Delete conftest.py

--- exercises/conftest.py
+++
@@ -1,4 +0,0 @@
-def pytest_sessionfinish(session, exitstatus):
-    """Prints a custom success message at the end of a successful test session."""
-    if exitstatus == 0:  # exitstatus 0 indicates all tests passed
-        print("\x1b[6;30;42m Success! Your code works as intended. \x1b[0m")
