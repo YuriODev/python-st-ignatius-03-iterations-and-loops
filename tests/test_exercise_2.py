f1076c928752a3bfb6359f70d5ad8c28849cc12c
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-23 21:53:58 +0000

    Update test_exercise_2.py

--- tests/test_exercise_2.py
+++ tests/test_exercise_2.py
@@ -1,28 +1,90 @@
 import unittest
-import subprocess
-import os
+from .test_utils import CustomTestCase, CustomTestRunner
 
 
-class TestExercise2(unittest.TestCase):
-    def run_exercise(self, input_value):
-        exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_2.py")
-        return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
+class TestExercise2(CustomTestCase):
 
     def test_m_times_n_10_5(self):
-        output = self.run_exercise("10\n5\n")
+        """
+        The program should print the number 10, 5 times.
+        """
+
+        inputs = ["10", "5"]
+        output = self.run_exercise(inputs)
         expected_output = "10 10 10 10 10 \n"
-        self.assertIn(expected_output, output)
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_m_times_n_10_10(self):
+        """
+        The program should print the number 10, 10 times.
+        """
+        inputs = ["10", "10"]
+        output = self.run_exercise(inputs)
+        expected_output = "10 10 10 10 10 10 10 10 10 10 \n"
+        self.assertInCustom(expected=expected_output,
+                            actual=output, input_value=inputs)
+
+    def test_m_times_n_5_3(self):
+        """
+        The program should print the number 5, 3 times.
+        """
+        inputs = ["5", "3"]
+        output = self.run_exercise(inputs)
+        expected_output = "5 5 5 \n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_m_times_n_7_7(self):
+        """
+        The program should print the number 7, 7 times.
+        """
+        inputs = ["7", "7"]
+        output = self.run_exercise(inputs)
+        expected_output = "7 7 7 7 7 7 7 \n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_m_times_n_1_1(self):
+        """
+        The program should print the number 1, 1 time.
+        """
+        inputs = ["1", "1"]
+        output = self.run_exercise(inputs)
+        expected_output = "1 \n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_m_times_n_0_0(self):
+        """
+        The program should print nothing.
+        """
+        inputs = ["0", "0"]
+        output = self.run_exercise(inputs)
+        expected_output = "\n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_m_times_n_3_3(self):
-        output = self.run_exercise("3\n3\n")
+        """
+        The program should print the number 3, 3 times.
+        """
+        inputs = ["3", "3"]
+        output = self.run_exercise(inputs)
         expected_output = "3 3 3 \n"
-        self.assertIn(expected_output, output)
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_m_times_n_7_1(self):
-        output = self.run_exercise("7\n1\n")
-        expected_output = "7\n"
-        self.assertIn(expected_output, output)
+        """
+        The program should print the number 7, 1 time.
+        """
+        inputs = ["7", "1"]
+        output = self.run_exercise(inputs)
+        expected_output = "7 \n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(testRunner=CustomTestRunner())
