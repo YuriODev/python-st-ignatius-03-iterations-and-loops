513f4f7148e05c41dcef01912addc0b768986cb7
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-23 23:05:54 +0000

    Move test

--- tests/test_exercise_4.py
+++ tests/test_exercise_4.py
@@ -1,27 +1,79 @@
-
 import unittest
-import subprocess
-import os
+from .test_utils import CustomTestCase, CustomTestRunner
 
-class TestExercise4(unittest.TestCase):
-    def run_exercise(self, input_value):
-        exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_4.py")
-        return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
 
-    def test_number_of_hashes_5(self):
-        output = self.run_exercise("5\n")
-        expected_output = "1 #\n2 ##\n3 ###\n4 ####\n5 #####\n"
-        self.assertEqual(output, expected_output)
+class TestExercise4(CustomTestCase):
+
+    def test_number_of_hashes_1(self):
+        """
+        The program should print the number of hashes according to the input.
+        """
+        inputs = ["1"]
+        output = self.run_exercise(inputs)
+        expected_output = "1 #\n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_number_of_hashes_2(self):
+        """
+        The program should print the number of hashes according to the input.
+        """
+        inputs = ["2"]
+        output = self.run_exercise(inputs)
+        expected_output = "1 #\n2 ##\n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_number_of_hashes_3(self):
-        output = self.run_exercise("3\n")
+        """
+        The program should print the number of hashes according to the input.
+        """
+        inputs = ["3"]
+        output = self.run_exercise(inputs)
         expected_output = "1 #\n2 ##\n3 ###\n"
-        self.assertEqual(output, expected_output)
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_number_of_hashes_4(self):
+        """
+        The program should print the number of hashes according to the input.
+        """
+        inputs = ["4"]
+        output = self.run_exercise(inputs)
+        expected_output = "1 #\n2 ##\n3 ###\n4 ####\n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_number_of_hashes_5(self):
+        """
+        The program should print the number of hashes according to the input.
+        """
+        inputs = ["5"]
+        output = self.run_exercise(inputs)
+        expected_output = "1 #\n2 ##\n3 ###\n4 ####\n5 #####\n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_number_of_hashes_6(self):
+        """
+        The program should print the number of hashes according to the input.
+        """
+        inputs = ["6"]
+        output = self.run_exercise(inputs)
+        expected_output = "1 #\n2 ##\n3 ###\n4 ####\n5 #####\n6 ######\n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_number_of_hashes_7(self):
-        output = self.run_exercise("7\n")
+        """
+        The program should print the number of hashes according to the input.
+        """
+        inputs = ["7"]
+        output = self.run_exercise(inputs)
         expected_output = "1 #\n2 ##\n3 ###\n4 ####\n5 #####\n6 ######\n7 #######\n"
-        self.assertEqual(output, expected_output)
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(testRunner=CustomTestRunner())
