02c1e35b60534fb5fe728e90f72e54a60aad65d9
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-23 20:56:13 +0000

    Update test_exercise_3.py

--- tests/test_exercise_3.py
+++ tests/test_exercise_3.py
@@ -8,19 +8,22 @@ class TestExercise3(CustomTestCase):
         inputs = ["25"]
         output = self.run_exercise(inputs)
         expected_output = "20 21 22 23 24 25"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_sequence_to_30(self):
         inputs = ["30"]
         output = self.run_exercise(inputs)
         expected_output = "20 21 22 23 24 25 26 27 28 29 30"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_sequence_to_20(self):
         inputs = ["20"]
         output = self.run_exercise(inputs)
         expected_output = "20"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
 
 if __name__ == '__main__':
