e4d193a7cc2ddb961bf92cc0a252fdac9c9716d1
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-24 20:36:42 +0000

    Update test_exercise_19.py

--- tests/test_exercise_19.py
+++ tests/test_exercise_19.py
@@ -1,48 +1,82 @@
-
 import unittest
-import subprocess
-import os
-import ast
-
-class TestExercise19(unittest.TestCase):
-    def run_exercise(self, input_value):
-        exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_19.py")
-        return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
-
-    def test_sum_of_squares_divisible_by_5(self):
-        output = self.run_exercise("5\n")
-        expected_numbers = [12, 13, 17, 18, 21, 24, 26, 29, 31, 34, 36, 39, 42, 43, 47, 48, 50, 55, 62, 63, 67, 68, 71, 74, 76, 79, 81, 84, 86, 89, 92, 93, 97, 98]
-        output_numbers = list(map(int, output.strip().split(", ")))
-        self.assertEqual(output_numbers, expected_numbers)
-
-    def test_sum_of_squares_divisible_by_7(self):
-        output = self.run_exercise("7\n")
-        expected_numbers = [70, 77]
-        output_numbers = list(map(int, output.strip().split(", ")))
-        self.assertEqual(output_numbers, expected_numbers)
-
-    def test_sum_of_squares_divisible_by_188(self):
-        output = self.run_exercise("188\n")
-        expected_numbers = [99]
-        output_numbers = list(map(int, output.strip().split(", ")))
-        self.assertEqual(output_numbers, expected_numbers)
-
-    def chek_list_usage(self):
-        with open("exercise_19.py", "r") as source_code:
-            tree = ast.parse(source_code.read())
-            for node in ast.walk(tree):
-                if isinstance(node, (ast.List, ast.ListComp)):
-                    self.fail("List usage found in the code.")
-                elif isinstance(node, ast.Call) and isinstance(node.func, ast.Name) and node.func.id == 'list':
-                    self.fail("List usage found in the code.")
-
-    def test_no_list_usage(self):
-        with open("exercise_19.py", "r") as source_code:
-            code = source_code.read()
-            self.assertNotIn("list", code)
-            self.assertNotIn("[", code)
-            self.assertNotIn("]", code)
+from .test_utils import CustomTestCase, CustomTestRunner
 
 
-if __name__ == '__main__':
-    unittest.main()
+class TestExercise19(CustomTestCase):
+
+    def test_loop_usage(self):
+        """
+        The program should use a 'for' or 'while' loop to solve the exercise.
+        """
+
+        self.assertUsesLoops()
+
+    def test_sum_of_squares_1(self):
+        """
+        The program should print a sequence of two-digit numbers, the sum of the squares of the digits of which is divisible by 5.
+        """
+
+        inputs = ["5"]
+        output = self.run_exercise(inputs)
+        expected_output = "12, 13, 17, 18, 21, 24, 26, 29, 31, 34, 36, 39, 42, 43, 47, 48, 50, 55, 62, 63, 67, 68, 71, 74, 76, 79, 81, 84, 86, 89, 92, 93, 97, 98, "
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_sum_of_squares_2(self):
+        """
+        The program should print a sequence of two-digit numbers, the sum of the squares of the digits of which is divisible by 7.
+        """
+
+        inputs = ["7"]
+        output = self.run_exercise(inputs)
+        expected_output = "70, 77, "
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_sum_of_squares_3(self):
+        """
+        The program should print a sequence of two-digit numbers, the sum of the squares of the digits of which is divisible by 10.
+        """
+
+        inputs = ["10"]
+        output = self.run_exercise(inputs)
+        expected_output = "13, 17, 24, 26, 31, 39, 42, 48, 55, 62, 68, 71, 79, 84, 86, 93, 97, "
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_sum_of_squares_4(self):
+        """
+        The program should print a sequence of two-digit numbers, the sum of the squares of the digits of which is divisible by 100.
+        """
+
+        inputs = ["100"]
+        output = self.run_exercise(inputs)
+        expected_output = "68, 86, "
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_sum_of_squares_5(self):
+        """
+        The program should print a sequence of two-digit numbers, the sum of the squares of the digits of which is divisible by 188.
+        """
+
+        inputs = ["162"]
+        output = self.run_exercise(inputs)
+        expected_output = "99, "
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_sum_of_squares_6(self):
+        """
+        The program should print a sequence of two-digit numbers, the sum of the squares of the digits of which is divisible by 1.
+        """
+
+        inputs = ["1"]
+        output = self.run_exercise(inputs)
+        expected_output = "10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, "
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+
+if __name__ == "__main__":
+    unittest.main(testRunner=CustomTestRunner)
