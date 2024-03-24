6021d121e7f9ebf3e47feb7b2b1ae0a85481eb42
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-24 18:02:08 +0000

    Update test_exercise_6.py

--- tests/test_exercise_6.py
+++ tests/test_exercise_6.py
@@ -1,27 +1,93 @@
-
 import unittest
-import subprocess
-import os
+from .test_utils import CustomTestCase, CustomTestRunner
 
-class TestExercise6(unittest.TestCase):
-    def run_exercise(self, input_value):
-        exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_6.py")
-        return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
 
-    def test_star_pattern_5(self):
-        output = self.run_exercise("5\n")
-        expected_output = "*\n**\n***\n****\n*****\n"
-        self.assertEqual(output, expected_output)
+class TestExercise6(CustomTestCase):
+
+    def test_loop_usage(self):
+        """
+        The program should use a 'for' or 'while' loop to solve the exercise.
+        """
+
+        self.assertUsesLoops()
+
+    def test_star_pattern_1(self):
+        """
+        The program should print a star pattern according to the input of 1.
+        """
+
+        inputs = ["1"]
+        output = self.run_exercise(inputs)
+        expected_output = "*\n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_star_pattern_2(self):
+        """
+        The program should print a star pattern according to the input of 2.
+        """
+
+        inputs = ["2"]
+        output = self.run_exercise(inputs)
+        expected_output = "*\n**\n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_star_pattern_3(self):
-        output = self.run_exercise("3\n")
+        """
+        The program should print a star pattern according to the input of 3.
+        """
+
+        inputs = ["3"]
+        output = self.run_exercise(inputs)
         expected_output = "*\n**\n***\n"
-        self.assertEqual(output, expected_output)
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_star_pattern_4(self):
+        """
+        The program should print a star pattern according to the input of 4.
+        """
+
+        inputs = ["4"]
+        output = self.run_exercise(inputs)
+        expected_output = "*\n**\n***\n****\n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_star_pattern_5(self):
+        """
+        The program should print a star pattern according to the input of 5.
+        """
+
+        inputs = ["5"]
+        output = self.run_exercise(inputs)
+        expected_output = "*\n**\n***\n****\n*****\n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+        
+    def test_star_pattern_6(self):
+        """
+        The program should print a star pattern according to the input of 6.
+        """
+
+        inputs = ["6"]
+        output = self.run_exercise(inputs)
+        expected_output = "*\n**\n***\n****\n*****\n******\n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_star_pattern_7(self):
-        output = self.run_exercise("7\n")
+        """
+        The program should print a star pattern according to the input of 7.
+        """
+
+        inputs = ["7"]
+        output = self.run_exercise(inputs)
         expected_output = "*\n**\n***\n****\n*****\n******\n*******\n"
-        self.assertEqual(output, expected_output)
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(testRunner=CustomTestRunner())
