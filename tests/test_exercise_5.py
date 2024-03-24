dd188d0e8ede02e4fe8c23ebf6978bad644790c1
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-24 17:50:37 +0000

    Update test_exercise_5.py

--- tests/test_exercise_5.py
+++ tests/test_exercise_5.py
@@ -1,35 +1,42 @@
-
 import unittest
-import subprocess
-import os
-import ast
+from .test_utils import CustomTestCase, CustomTestRunner
 
-class TestExercise5(unittest.TestCase):
-    def run_exercise(self, input_value):
-        exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_5.py")
-        return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
+
+class TestExercise5(CustomTestCase):
+
+    def test_loop_usage(self):
+        self.assertUsesLoops()
 
     def test_sum_of_integers_100(self):
-        output = self.run_exercise("100\n")
+        """
+        The program should print the sum of all integers from 1 to 100.
+        """
+        inputs = ["100"]
+        output = self.run_exercise(inputs)
         expected_output = "5050\n"
-        self.assertEqual(output, expected_output)
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_sum_of_integers_16(self):
-        output = self.run_exercise("16\n")
+        """
+        The program should print the sum of all integers from 1 to 16.
+        """
+        inputs = ["16"]
+        output = self.run_exercise(inputs)
         expected_output = "136\n"
-        self.assertEqual(output, expected_output)
-
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+        
     def test_sum_of_integers_1(self):
-        output = self.run_exercise("1\n")
+        """
+        The program should print the sum of all integers from 1 to 1.
+        """
+        inputs = ["1"]
+        output = self.run_exercise(inputs)
         expected_output = "1\n"
-        self.assertEqual(output, expected_output)
-
-    def test_no_loop_usage(self):
-        with open(os.path.join(os.path.dirname(__file__), "exercise_5.py"), "r") as file:
-            content = file.read()
-            print(content)
-            self.assertTrue(("for" in content) or ("while" in content), "You should use a loop for this exercise")
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(testRunner=CustomTestRunner())
