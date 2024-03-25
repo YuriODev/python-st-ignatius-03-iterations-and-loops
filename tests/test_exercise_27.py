3c4f234fc1988cf1854a748c6eb57a3f3bafdedf
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-25 05:19:30 +0000

    Update test_exercise_27.py

--- tests/test_exercise_27.py
+++ tests/test_exercise_27.py
@@ -1,16 +1,77 @@
-
 import unittest
-import subprocess
-import os
+from .test_utils import CustomTestCase, CustomTestRunner
 
-class TestExercise27(unittest.TestCase):
-    def run_exercise(self, input_value):
-        exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_27.py")
-        return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
 
-    def test_sum_of_a_series_10(self):
-        output = self.run_exercise("10\n")
-        self.assertAlmostEqual(float(output.strip()), 3.0418396189294032, places=5)
+class TestExercise27(CustomTestCase):
+
+        def test_loop_usage(self):
+            """
+            The program should use a 'for' or 'while' loop to solve the exercise.
+            """
+
+            self.assertUsesLoops()
+
+        def test_sum_of_series(self):
+            """
+            The program should calculate the sum of the first `n` terms of the series `Pi = 4/1 - 4/3 + 4/5 - 4/7 + ...`.
+            """
+
+            inputs = ["5"]
+            output = self.run_exercise(inputs)
+            expected_output = "3.3396825396825403\n"
+
+            self.assertAlmostEqualCustom(expected=expected_output,
+                                         input_value=inputs,
+                                         actual=output, places=3)
+
+        def test_sum_of_series_2(self):
+            """
+            The program should calculate the sum of the first `n` terms of the series `Pi = 4/1 - 4/3 + 4/5 - 4/7 + ...`.
+            """
+
+            inputs = ["10"]
+            output = self.run_exercise(inputs)
+            expected_output = "3.3396825396825403\n"
+            self.assertAlmostEqualCustom(expected=expected_output,
+                                         input_value=inputs,
+                                         actual=output, places=3)
+
+        def test_sum_of_series_3(self):
+            """
+            The program should calculate the sum of the first `n` terms of the series `Pi = 4/1 - 4/3 + 4/5 - 4/7 + ...`.
+            """
+
+            inputs = ["15"]
+            output = self.run_exercise(inputs)
+            expected_output = "3.017071817071818\n"
+            self.assertAlmostEqualCustom(expected=expected_output,
+                                         input_value=inputs,
+                                         actual=output, places=3)
+
+        def test_sum_of_series_4(self):
+            """
+            The program should calculate the sum of the first `n` terms of the series `Pi = 4/1 - 4/3 + 4/5 - 4/7 + ...`.
+            """
+
+            inputs = ["120"]
+            output = self.run_exercise(inputs)
+            expected_output = "3.1332594798865546\n"
+            self.assertAlmostEqualCustom(expected=expected_output,
+                                         input_value=inputs,
+                                         actual=output, places=3)
+
+        def test_sum_of_series_5(self):
+            """
+            The program should calculate the sum of the first `n` terms of the series `Pi = 4/1 - 4/3 + 4/5 - 4/7 + ...`.
+            """
+
+            inputs = ["500"]
+            output = self.run_exercise(inputs)
+            expected_output = "3.139592655589783\n"
+            self.assertAlmostEqualCustom(expected=expected_output,
+                                         input_value=inputs,
+                                         actual=output, places=3)
+
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(testRunner=CustomTestRunner())
