8468eb32c13584ae8cba1cd826b7ecb1f3da4c88
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-01 20:04:05 -0300

    Update test_exercise_28.py

--- tests/test_exercise_28.py
+++ tests/test_exercise_28.py
@@ -1,74 +1,3 @@
-# # ## Exercise 28: Product of Two Numbers - Medium 🔥 (Est. Time: 10-15 mins | Points: 20)
-
-# # **Problem:** Given integers a and b. Find their product without using the multiplication operation.
-
-# # ### Input:
-
-# # - Two integers `a` and `b`.
-
-# # ### Output:
-
-# # - An integer representing the product of `a` and `b`.
-
-# # ### Examples:
-
-# # | No. | Inputs | Outputs |
-# # | --- | ------ | ------- |
-# # | 1   | 7<br> 8 | 56 |
-# # | 2 | 5<br> 6 | 30 |
-# # | 3 | 3<br> 4 | 12 |
-# # | 4 | 10<br> 10 | 100 |
-
-# # ### Note:
-
-# # The problem tests the ability to use loops and conditional statements to calculate the product of two numbers.
-
-# # Your solution to Exercise 28
-
-# # Prompt the user to enter two integers, `a` and `b`.
-# a = int(input())
-# b = int(input())
-
-# # Initialize the product
-# product = 0
-
-# # Calculate the product of a and b
-# # If both a and b are negative
-# if a < 0 and b < 0:
-#     # Convert them to positive and
-#     a = abs(a)
-#     b = abs(b)
-#     # Add b to the product a times.
-#     for i in range(a):
-#         product += b
-
-# # If a is negative
-# elif a < 0:
-#     # Convert a to positive
-#     a = abs(a)
-#     # Add a to the product b times.
-#     for i in range(a):
-#         product += b
-#     product = -product
-
-# # If b is negative
-# elif b < 0:
-#     # Convert b to positive
-#     b = abs(b)
-#     # Add b to the product a times.
-#     for i in range(b):
-#         product += a
-#     product = -product
-
-# # If both a and b are positive
-# else:
-#     # Add b to the product a times.
-#     for i in range(a):
-#         product += b
-
-# # Output the product  
-# print(product)
-
 import unittest
 from .test_utils import CustomTestCase, CustomTestRunner
 
@@ -132,7 +61,7 @@ class TestExercise28(CustomTestCase):
         expected_output = "100\n"
         self.assertInCustom(expected=expected_output, actual=output,
                             input_value=inputs) 
-        
+
 
 if __name__ == '__main__':
     unittest.main(testRunner=CustomTestRunner())
