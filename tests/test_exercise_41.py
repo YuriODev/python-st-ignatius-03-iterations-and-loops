6da39a3625b4b345fc9197f155662415a5fee83f
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-06 11:35:32 -0300

    Update test_exercise_41.py

--- tests/test_exercise_41.py
+++ tests/test_exercise_41.py
@@ -1,53 +1,81 @@
 import unittest
-import subprocess
-import os
-import ast
+from .test_utils import CustomTestCase, CustomTestRunner
 
-class TestExercise41(unittest.TestCase):
-    def run_exercise(self, input_value):
-        exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_41.py")
-        return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
 
-    def test_fibonacci_sequence_number(self):
-        output = self.run_exercise("9\\n")
-        self.assertEqual(int(output.strip()), 34)
+class TestExercise41(CustomTestCase):
 
-    def test_fibonacci_sequence_number_2(self):
-        output = self.run_exercise("10\\n")
-        self.assertEqual(int(output.strip()), 55)
-
-    def test_fibonacci_sequence_number_3(self):
-        output = self.run_exercise("11\\n")
-        self.assertEqual(int(output.strip()), 89)
-
-    def test_fibonacci_sequence_number_4(self):
-        output = self.run_exercise("12\\n")
-        self.assertEqual(int(output.strip()), 144)
-
-    def test_fibonacci_sequence_number_5(self):
-        output = self.run_exercise("13\\n")
-        self.assertEqual(int(output.strip()), 233)
-
-    def test_fibonacci_sequence_number_6(self):
-        output = self.run_exercise("14\\n")
-        self.assertEqual(int(output.strip()), 377)
-
-    def chek_list_usage(self):
-        with open("exercise_41.py", "r") as source_code:
-            tree = ast.parse(source_code.read())
-            for node in ast.walk(tree):
-                if isinstance(node, (ast.List, ast.ListComp)):
-                    self.fail("List usage found in the code.")
-                elif isinstance(node, ast.Call) and isinstance(node.func, ast.Name) and node.func.id == 'list':
-                    self.fail("List usage found in the code.")
+    def test_loop_usage(self):
+        """
+        The program should use a 'for' or 'while' loop to solve the exercise.
+        """
 
     def test_no_list_usage(self):
-        with open("exercise_41.py", "r") as source_code:
-            code = source_code.read()
-            self.assertNotIn("list", code)
-            self.assertNotIn("[", code)
-            self.assertNotIn("]", code)
+        """
+        The program should not use string slicing to solve the exercise.
+        """
+
+        self.assertNotUsesList()
+
+    def test_fibonacci_sequence_to_50(self):
+        """
+        The Fibonacci number 9 is 34
+        """
+        inputs = ["9"]
+        output = self.run_exercise(inputs)
+        expected_output = "34"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_fibonacci_sequence_number_2(self):
+        """
+        The Fibonacci number 10 is 55
+        """
+        inputs = ["10"]
+        output = self.run_exercise(inputs)
+        expected_output = "55"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_fibonacci_sequence_number_3(self):
+        """
+        The Fibonacci number 11 is 89
+        """
+        inputs = ["11"]
+        output = self.run_exercise(inputs)
+        expected_output = "89"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_fibonacci_sequence_number_4(self):
+        """
+        The Fibonacci number 12 is 144
+        """
+        inputs = ["12"]
+        output = self.run_exercise(inputs)
+        expected_output = "144"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_fibonacci_sequence_number_5(self):
+        """
+        The Fibonacci number 13 is 233
+        """
+        inputs = ["13"]
+        output = self.run_exercise(inputs)
+        expected_output = "233"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_fibonacci_sequence_number_6(self):
+        """
+        The Fibonacci number 14 is 377
+        """
+        inputs = ["14"]
+        output = self.run_exercise(inputs)
+        expected_output = "377"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(testRunner=CustomTestRunner)
