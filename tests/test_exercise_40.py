fcf48181d1de388500aee7c944aa8ca5cc83adbe
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-06 11:30:05 -0300

    Update test_exercise_40.py

--- tests/test_exercise_40.py
+++ tests/test_exercise_40.py
@@ -1,55 +1,70 @@
-
 import unittest
-import subprocess
-import os
-import ast
+from .test_utils import CustomTestCase, CustomTestRunner
 
-class TestExercise40(unittest.TestCase):
-    def run_exercise(self, input_value):
-        exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_40.py")
-        return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
+
+class TestExercise40(CustomTestCase):
+
+    def test_loop_usage(self):
+        """
+        The program should use a 'for' or 'while' loop to solve the exercise.
+        """
+
+    def test_no_list_usage(self):
+        """
+        The program should not use string slicing to solve the exercise.
+        """
+
+        self.assertNotUsesList()
 
     def test_fibonacci_sequence_to_50(self):
-        output = self.run_exercise("50\n")
-        expected_sequence = "1 1 2 3 5 8 13 21 34"
-        self.assertIn(expected_sequence, output)
+        """
+        The Fibonacci numbers less than 50 are: 1 1 2 3 5 8 13 21 34
+        """
+        inputs = ["50"]
+        output = self.run_exercise(inputs)
+        expected_output = "1 1 2 3 5 8 13 21 34"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_fibonacci_sequence_to_100(self):
-        output = self.run_exercise("100\n")
-        expected_sequence = "1 1 2 3 5 8 13 21 34 55 89"
-        self.assertIn(expected_sequence, output)
+        """
+        The Fibonacci numbers less than 100 are: 1 1 2 3 5 8 13 21 34 55 89
+        """
+        inputs = ["100"]
+        output = self.run_exercise(inputs)
+        expected_output = "1 1 2 3 5 8 13 21 34 55 89"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_fibonacci_sequence_to_200(self):
-        output = self.run_exercise("200\n")
-        expected_sequence = "1 1 2 3 5 8 13 21 34 55 89 144"
-        self.assertIn(expected_sequence, output)
+        """
+        The Fibonacci numbers less than 200 are: 1 1 2 3 5 8 13 21 34 55 89 144
+        """
+        inputs = ["200"]
+        output = self.run_exercise(inputs)
+        expected_output = "1 1 2 3 5 8 13 21 34 55 89 144"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_fibonacci_sequence_to_5(self):
-        output = self.run_exercise("5\n")
-        expected_sequence = "1 1 2 3 5"
+        """
+        The Fibonacci numbers less than 5 are: 1 1 2 3
+        """
+
+        inputs = ["5"]
+        output = self.run_exercise(inputs)
+        expected_sequence = "1 1 2 3"
         self.assertIn(expected_sequence, output)
 
     def test_fibonacci_sequence_to_10(self):
-        output = self.run_exercise("10\n")
+        """
+        The Fibonacci numbers less than 10 are: 1 1 2 3 5 8
+        """
+        inputs = ["10"]
+        output = self.run_exercise(inputs)
         expected_sequence = "1 1 2 3 5 8"
         self.assertIn(expected_sequence, output)
 
-    def chek_list_usage(self):
-        with open("exercise_40.py", "r") as source_code:
-            tree = ast.parse(source_code.read())
-            for node in ast.walk(tree):
-                if isinstance(node, (ast.List, ast.ListComp)):
-                    self.fail("List usage found in the code.")
-                elif isinstance(node, ast.Call) and isinstance(node.func, ast.Name) and node.func.id == 'list':
-                    self.fail("List usage found in the code.")
 
-    def test_no_list_usage(self):
-        with open("exercise_40.py", "r") as source_code:
-            code = source_code.read()
-            self.assertNotIn("list", code)
-            self.assertNotIn("[", code)
-            self.assertNotIn("]", code)
-
-
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    unittest.main(testRunner=CustomTestRunner)
