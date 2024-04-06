18ffb2f40e0e2620a818c4342d339723723446a2
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-06 17:11:20 -0300

    Update test_exercise_45.py

--- tests/test_exercise_45.py
+++ tests/test_exercise_45.py
@@ -1,54 +1,91 @@
 import unittest
-import subprocess
-import os
-import ast
+from .test_utils import CustomTestCase, CustomTestRunner
 
 
-class TestExercise45(unittest.TestCase):
-    def run_exercise(self, input_values):
-        exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_45.py")
-        return subprocess.check_output(['python3', exercise_file_path], input=input_values, text=True, universal_newlines=True)
+class TestExercise45(CustomTestCase):
 
-    def test_sign_changes(self):
-        output = self.run_exercise("-5\\n-3\\n10\\n6\\n-4\\n7\\n-1\\n0\\n")
-        self.assertEqual(int(output.strip()), 4)
-
-    def test_no_sign_changes(self):
-        output = self.run_exercise("5\\n3\\n10\\n6\\n4\\n7\\n1\\n0\\n")
-        self.assertEqual(int(output.strip()), 0)
-
-    def test_sign_changes_2(self):
-        output = self.run_exercise("5\\n3\\n10\\n6\\n-4\\n7\\n1\\n0\\n")
-        self.assertEqual(int(output.strip()), 3)
-
-    def test_sign_changes_3(self):
-        output = self.run_exercise("-5\\n3\\n10\\n6\\n-4\\n7\\n1\\n0\\n")
-        self.assertEqual(int(output.strip()), 3)
-
-    def test_sign_changes_4(self):
-        output = self.run_exercise("-5\\n-3\\n10\\n6\\n4\\n7\\n1\\n0\\n")
-        self.assertEqual(int(output.strip()), 5)
-
-    def test_sign_changes_5(self):
-        output = self.run_exercise("-5\\n-3\\n-10\\n-6\\n-4\\n-7\\n-1\\n0\\n")
-        self.assertEqual(int(output.strip()), 0)
-
-    def chek_list_usage(self):
-        with open("exercise_45.py", "r") as source_code:
-            tree = ast.parse(source_code.read())
-            for node in ast.walk(tree):
-                if isinstance(node, (ast.List, ast.ListComp)):
-                    self.fail("List usage found in the code.")
-                elif isinstance(node, ast.Call) and isinstance(node.func, ast.Name) and node.func.id == 'list':
-                    self.fail("List usage found in the code.")
+    def test_loop_usage(self):
+        """
+        The program should use a 'for' or 'while' loop to solve the exercise.
+        """
 
     def test_no_list_usage(self):
-        with open("exercise_45.py", "r") as source_code:
-            code = source_code.read()
-            self.assertNotIn("list", code)
-            self.assertNotIn("[", code)
-            self.assertNotIn("]", code)
+        """
+        The program should not use string slicing to solve the exercise.
+        """
+
+        self.assertNotUsesList()
+
+    def test_index_of_largest_element(self):
+        """
+        The sign changes in the sequence -5, -3, 10, 6, -4, 7, -1, 0 is 4
+        """
+        inputs = ["-5", "-3", "10", "6", "-4", "7", "-1", "0"]
+        output = self.run_exercise(inputs)
+        expected_output = "4"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_no_sign_changes(self):
+        """
+        The sign changes in the sequence 1, 2, 3, 4, 5, 6, 7, 8 is 0
+        """
+        inputs = ["1", "2", "3", "4", "5", "6", "7", "0"]
+        output = self.run_exercise(inputs)
+        expected_output = "0"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_no_sign_changes_2(self):
+        """
+        The sign changes in the sequence 1, 2, 3, 4, 5, 6, 7, 8 is 0
+        """
+        inputs = ["-1", "-2", "-3", "-4", "-5", "-6", "-7", "0"]
+        output = self.run_exercise(inputs)
+        expected_output = "0"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_sign_changes_2(self):
+        """
+        The sign changes in the sequence 5, 3, 10, 6, -4, 7, 1, 0 is 3
+        """
+        inputs = ["5", "3", "10", "6", "-4", "7", "1", "0"]
+        output = self.run_exercise(inputs)
+        expected_output = "2"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_sign_changes_3(self):
+        """
+        The sign changes in the sequence -5, 3, 10, 6, -4, 7, 1, 0 is 3
+        """
+        inputs = ["-5", "3", "10", "6", "-4", "7", "1", "0"]
+        output = self.run_exercise(inputs)
+        expected_output = "3"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_sign_changes_4(self):
+        """
+        The sign changes in the sequence -5, -3, -10, -6, -4, -7, -1, 0 is 0
+        """
+        inputs = ["-5", "-3", "-10", "-6", "-4", "-7", "-1", "0"]
+        output = self.run_exercise(inputs)
+        expected_output = "0"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_sign_changes_5(self):
+        """
+        The sign changes in the sequence 5, 3, 10, 6, 4, 7, 1, 0 is 0
+        """
+        inputs = ["5", "3", "10", "6", "4", "7", "1", "0"]
+        output = self.run_exercise(inputs)
+        expected_output = "0"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(testRunner=CustomTestRunner)
