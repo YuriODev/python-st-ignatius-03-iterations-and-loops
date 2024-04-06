2c8f44e74327cffeba10d44ecd82a77b6d4f5c1e
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-06 10:04:42 -0300

    Update test_exercise_37.py

--- tests/test_exercise_37.py
+++ tests/test_exercise_37.py
@@ -1,40 +1,73 @@
-
 import unittest
-import subprocess
-import os
-import ast
+from .test_utils import CustomTestCase, CustomTestRunner
 
-class TestExercise37(unittest.TestCase):
-    def run_exercise(self, input_values):
-        exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_37.py")
-        return subprocess.check_output(['python3', exercise_file_path], input=input_values, text=True, universal_newlines=True)
+
+class TestExercise37(CustomTestCase):
+
+    def test_loop_usage(self):
+        """
+        The program should use a 'for' or 'while' loop to solve the exercise.
+        """
+
+        self.assertUsesLoops()
+
+    def test_no_math_module_usage(self):
+        """
+        The program should not use the 'math' module.
+        """
+
+        self.assetNotUseMathModule()
 
     def test_division_without_division(self):
-        output = self.run_exercise("16\n5\n")
-        results = output.strip().split()
-        self.assertEqual(results, ['3', '1'])
-
+        """
+        The program's output for inputs 16 and 5 should be 3 and 1.
+        """
+        inputs = ["16", "5"]
+        output = self.run_exercise(inputs)
+        expected_output = "3 1\n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+        
     def test_division_without_division_2(self):
-        output = self.run_exercise("100\n3\n")
-        results = output.strip().split()
-        self.assertEqual(results, ['33', '1'])
+        """
+        The program's output for inputs 100 and 25 should be 4 and 0.
+        """
+        inputs = ["100", "25"]
+        output = self.run_exercise(inputs)
+        expected_output = "4 0\n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_division_without_division_3(self):
-        output = self.run_exercise("100\n25\n")
-        results = output.strip().split()
-        self.assertEqual(results, ['4', '0'])
+        """
+        The program's output for inputs 100 and 75 should be 1 and 25.
+        """
+        inputs = ["100", "75"]
+        output = self.run_exercise(inputs)
+        expected_output = "1 25\n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
-    def check_module_usage(self):
-        with open("exercise_37.py", "r") as source_code:
-            code = source_code.read()
-            self.assertNotIn("import", code)
-            self.assertNotIn("from", code)
+    def test_division_without_division_4(self):
+        """
+        The program's output for inputs 100 and 99 should be 1 and 1.
+        """
+        inputs = ["100", "99"]
+        output = self.run_exercise(inputs)
+        expected_output = "1 1\n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
-    def check_division_operator_usage(self):
-        with open("exercise_37.py", "r") as source_code:
-            code = source_code.read()
-            self.assertNotIn("/", code)
+    def test_division_without_division_5(self):
+        """
+        The program's output for inputs 100 and 100 should be 1 and 0.
+        """
+        inputs = ["100", "100"]
+        output = self.run_exercise(inputs)
+        expected_output = "1 0\n"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(testRunner=CustomTestRunner)
