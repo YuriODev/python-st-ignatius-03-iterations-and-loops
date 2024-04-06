8a4c97a8d6a39d43a181032572a6e6604df3a4ef
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-06 17:40:18 -0300

    Update test_exercise_49.py

--- tests/test_exercise_49.py
+++ tests/test_exercise_49.py
@@ -1,52 +1,68 @@
 import unittest
-import subprocess
-import os
-import ast
+from .test_utils import CustomTestCase, CustomTestRunner
 
 
-class TestExercise49(unittest.TestCase):
-    def run_exercise(self, input_value):
-        exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_49.py")
-        return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
+class TestExercise49(CustomTestCase):
 
-    def test_palindromes_1400_2200(self):
-        output = self.run_exercise("1400\n2200\n")
-        self.assertEqual(output.strip(), "1441 1551 1661 1771 1881 1991 2002 2112")
-
-    def test_palindromes_1000_2000(self):
-        output = self.run_exercise("1000\n2000\n")
-        self.assertEqual(output.strip(), "1001 1111 1221 1331 1441 1551 1661 1771 1881 1991")
-
-    def test_palindromes_2000_3000(self):
-        output = self.run_exercise("2000\n3000\n")
-        self.assertEqual(output.strip(), "2002 2112 2222 2332 2442 2552 2662 2772 2882 2992")
-
-    def check_list_usage(self):
-        with open("exercise_49.py", "r") as source_code:
-            tree = ast.parse(source_code.read())
-            for node in ast.walk(tree):
-                if isinstance(node, (ast.List, ast.ListComp)):
-                    self.fail("List usage found in the code.")
-                elif isinstance(node, ast.Call) and isinstance(node.func, ast.Name) and node.func.id == 'list':
-                    self.fail("List usage found in the code.")
+    def test_loop_usage(self):
+        """
+        The program should use a 'for' or 'while' loop to solve the exercise.
+        """
 
     def test_no_list_usage(self):
-        with open("exercise_49.py", "r") as source_code:
-            code = source_code.read()
-            self.assertNotIn("list", code)
-            self.assertNotIn("[", code)
-            self.assertNotIn("]", code)
+        """
+        The program should not use string slicing to solve the exercise.
+        """
 
-    def test_reverse_usage(self):
-        with open("exercise_49.py", "r") as source_code:
-            code = source_code.read()
-            self.assertNotIn("reverse", code)
+        self.assertNotUsesList()
 
-    def test_string_slice_usage_reverse(self):
-        with open("exercise_49.py", "r") as source_code:
-            code = source_code.read()
-            self.assertNotIn("[::-1]", code)
+    def test_not_string_conversion(self):
+        """
+        The program should not convert the input to a string.
+        """
+
+        self.assertNotUseStringSlice()
+
+    def test_palindromes_1400_2200(self):
+        """
+        Palindromes between 1400 and 2200
+        """
+        inputs = ["1400", "2200"]
+        output = self.run_exercise(inputs)
+        expected_output = "1441 1551 1661 1771 1881 1991 2002 2112"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_palindromes_1000_2000(self):
+        """
+        Palindromes between 1000 and 2000
+        """
+        inputs = ["1000", "2000"]
+        output = self.run_exercise(inputs)
+        expected_output = "1001 1111 1221 1331 1441 1551 1661 1771 1881 1991"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_palindromes_2000_3000(self):
+        """
+        Palindromes between 2000 and 3000
+        """
+        inputs = ["2000", "3000"]
+        output = self.run_exercise(inputs)
+        expected_output = "2002 2112 2222 2332 2442 2552 2662 2772 2882 2992"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_palindromes_4000_5000(self):
+        """
+        Palindromes between 4000 and 5000
+        """
+        inputs = ["4000", "5000"]
+        output = self.run_exercise(inputs)
+        expected_output = "4004 4114 4224 4334 4444 4554 4664 4774 4884 4994"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(testRunner=CustomTestRunner)
