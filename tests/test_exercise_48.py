dfc630e04452e623c063ca0533a433552c40dc27
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-06 17:28:16 -0300

    Update test_exercise_48.py

--- tests/test_exercise_48.py
+++ tests/test_exercise_48.py
@@ -1,51 +1,68 @@
 import unittest
-import subprocess
-import os
-import ast
+from .test_utils import CustomTestCase, CustomTestRunner
 
 
-class TestExercise48(unittest.TestCase):
-    def run_exercise(self, input_value):
-        exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_48.py")
-        return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
+class TestExercise48(CustomTestCase):
 
-    def test_palindromes_50(self):
-        output = self.run_exercise("50\n")
-        self.assertEqual(output.strip(), "13")
-
-    def test_palindromes_100(self):
-        output = self.run_exercise("100\n")
-        self.assertEqual(output.strip(), "18")
-
-    def test_palindromes_200(self):
-        output = self.run_exercise("200\n")
-        self.assertEqual(output.strip(), "27")
-
-    def check_list_usage(self):
-        with open("exercise_48.py", "r") as source_code:
-            tree = ast.parse(source_code.read())
-            for node in ast.walk(tree):
-                if isinstance(node, (ast.List, ast.ListComp)):
-                    self.fail("List usage found in the code.")
-                elif isinstance(node, ast.Call) and isinstance(node.func, ast.Name) and node.func.id == 'list':
-                    self.fail("List usage found in the code.")
+    def test_loop_usage(self):
+        """
+        The program should use a 'for' or 'while' loop to solve the exercise.
+        """
 
     def test_no_list_usage(self):
-        with open("exercise_48.py", "r") as source_code:
-            code = source_code.read()
-            self.assertNotIn("list", code)
-            self.assertNotIn("[", code)
-            self.assertNotIn("]", code)
+        """
+        The program should not use string slicing to solve the exercise.
+        """
 
-    def test_reverse_usage(self):
-        with open("exercise_48.py", "r") as source_code:
-            code = source_code.read()
-            self.assertNotIn("reverse", code)
+        self.assertNotUsesList()
 
-    def test_string_slice_usage_reverse(self):
-        with open("exercise_48.py", "r") as source_code:
-            code = source_code.read()
-            self.assertNotIn("[::-1]", code)
+    def test_not_string_conversion(self):
+        """
+        The program should not convert the input to a string.
+        """
+
+        self.assertNotUseStringSlice()
+
+    def test_palindromes_50(self):
+        """
+        Number of the palindromes between 1 and 50 is 13
+        """
+        inputs = ["50"]
+        output = self.run_exercise(inputs)
+        expected_output = "13"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_palindromes_100(self):
+        """
+        Number of the palindromes between 1 and 100 is 18
+        """
+        inputs = ["100"]
+        output = self.run_exercise(inputs)
+        expected_output = "18"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+    
+    def test_palindromes_200(self):
+        """
+        Number of the palindromes between 1 and 200 is 28
+        """
+        inputs = ["200"]
+        output = self.run_exercise(inputs)
+        expected_output = "28"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_palindromes_1000(self):
+        """
+        Number of the palindromes between 1 and 1000 is 108
+        """
+        inputs = ["1000"]
+        output = self.run_exercise(inputs)
+        expected_output = "108"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(testRunner=CustomTestRunner)
