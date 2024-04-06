147a3890c643d8a33d081fed2d3e04729368d212
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-06 18:01:31 -0300

    Update test_exercise_51.py

--- tests/test_exercise_51.py
+++ tests/test_exercise_51.py
@@ -1,63 +1,68 @@
-# ## Exercise 51: Three Identical Digits - Hard 🥵 (Est. Time: 15-20 mins | Points: 30)
-
-# **Problem:** Two four-digit numbers `a` and `b` are entered. Print all four-digit numbers in the interval from `a` to `b` that contain only three identical digits.
-
-# ### Input:
-
-# - Two four-digit numbers `a` and `b`.
-
-# ### Output:
-
-# - A sequence of four-digit numbers in the interval from `a` to `b` that contain only three identical digits.
-
-# ### Examples:
-
-# | No. | Inputs | Outputs |
-# | --- | ------ | ------- |
-# | 1   | 1400<br>1600 | 1411 1444 1511 1555 |
-# | 2   | 1000<br>2000 | 1111 1222 1333 1444 1555 1666 1777 1888 1999 |
-# | 3   | 2000<br>3000 | 2002 2111 2222 2333 2444 2555 2666 2777 2888 2999 |
-
 import unittest
-import subprocess
-import os
-import ast
+from .test_utils import CustomTestCase, CustomTestRunner
 
 
-class TestExercise51(unittest.TestCase):
-    def run_exercise(self, input_value):
-        exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_51.py")
-        return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
+class TestExercise51(CustomTestCase):
 
-    def test_three_identical_digits_1400_1600(self):
-        output = self.run_exercise("1400\n1600\n")
-        self.assertEqual(output.strip(), "1411 1444 1511 1555")
+    def test_loop_usage(self):
+        """
+        The program should use a 'for' or 'while' loop to solve the exercise.
+        """
 
-    def test_three_identical_digits_1000_2000(self):
-        output = self.run_exercise("1000\n2000\n")
-        self.assertEqual(output.strip(), "1111 1222 1333 1444 1555 1666 1777 1888 1999")
+    def test_no_list_usage(self):
+        """
+        The program should not use string slicing to solve the exercise.
+        """
 
-    def test_three_identical_digits_2000_3000(self):
-        output = self.run_exercise("2000\n3000\n")
-        self.assertEqual(output.strip(), "2002 2111 2222 2333 2444 2555 2666 2777 2888 2999")
+        self.assertNotUsesList()
 
-    def check_list_usage(self):
-        with open("exercise_51.py", "r") as source_code:
-            tree = ast.parse(source_code.read())
-            for node in ast.walk(tree):
-                if isinstance(node, (ast.List, ast.ListComp)):
-                    self.fail("List usage found in the code.")
-                elif isinstance(node, ast.Call) and isinstance(node.func, ast.Name) and node.func.id == 'list':
-                    self.fail("List usage found in the code.")
+    def test_not_string_conversion(self):
+        """
+        The program should not convert the input to a string.
+        """
 
-    def test_no_list_str_usage(self):
-        with open("exercise_51.py", "r") as source_code:
-            code = source_code.read()
-            self.assertNotIn("list", code)
-            self.assertNotIn("str", code)
-            self.assertNotIn("[", code)
-            self.assertNotIn("]", code)
+        self.assertNotUseStringSlice()
+
+    def test_three_identical_digits_1000_1200(self):
+        """
+        Numbers with 3 identical digits between 1000 and 1200: 1000 1011 1101 1111 1121 1131 1141 1151 1161 1171 1181 1191
+        """
+        inputs = ["1000", "1200"]
+        output = self.run_exercise(inputs)
+        expected_output = "1000 1011 1101 1110 1111 1112 1113 1114 1115 1116 1117 1118 1119 1121 1131 1141 1151 1161 1171 1181 1191"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_three_identical_digits_2169_2190(self):
+        """
+        Numbers with 3 identical digits between 2169 and 2190: 2177 2188 2199
+        """
+        inputs = ["2169", "2290"]
+        output = self.run_exercise(inputs)
+        expected_output = "2220"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_three_identical_digits_3000_3100(self):
+        """
+        Numbers with 3 identical digits between 3000 and 3100: 3000 3033
+        """
+        inputs = ["3000", "3100"]
+        output = self.run_exercise(inputs)
+        expected_output = "3000 3033"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_three_identical_digits_9000_9999(self):
+        """
+        Numbers with 3 identical digits between 9000 and 9999: 9000 9111 9222 9333 9444 9555 9666 9777 9888 9999
+        """
+        inputs = ["9000", "9999"]
+        output = self.run_exercise(inputs)
+        expected_output = "9000 9099 9111 9199 9222 9299 9333 9399 9444 9499 9555 9599 9666 9699 9777 9799 9888 9899 9909 9919 9929 9939 9949 9959 9969 9979 9989 9990 9991 9992 9993 9994 9995 9996 9997 9998"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(testRunner=CustomTestRunner)
