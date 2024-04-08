b3fa84afb4afdabf6faf16147dc894fda3d803c1
Yurii Oksamytnyi <141469505+YuriiOks@users.noreply.github.com>
2024-04-08 16:43:35 +0100

    Update exercise_10.py

--- exercises/exercise_10.py
+++ exercises/exercise_10.py
@@ -1,10 +1,1 @@
-# Prompt the user to enter the number of pounds.
-pounds = int(input())
-
-# Initialize the kilograms variable.
-kilograms = 0
-
-# Loop through the range of pounds and print the equivalent kilograms.
-for i in range(1, pounds + 1):
-    kilograms += 0.45
-    print(f"{i} {kilograms:.2f}")
+# Your solution to Exercise 10
