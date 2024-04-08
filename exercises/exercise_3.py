814afc4ee9e298c9c3a8fa5fe6a6e5bd4002171f
Yurii Oksamytnyi <141469505+YuriiOks@users.noreply.github.com>
2024-04-08 16:43:54 +0100

    Update exercise_3.py

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,12 +1,1 @@
-# Prompt the user to enter a number
-n = int(input())
-
-# Iterate over a range of numbers from 20 to n+1
-for i in range(20, n+1):
-    # Check if the current number is equal to n
-    if i == n:
-        # If it is, print the number
-        print(i)
-    else:
-        # If it's not, print the number followed by a space
-        print(i, end=" ")
+# Your solution to Exercise 3
